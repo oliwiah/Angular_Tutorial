e2d95c7143f63f909daf01b26a6b819a317830ad
Oliwia Hajszprych <oliwia.hajszprych@gmail.com>
2018-06-11 12:06:16 +0200

    task section 5 done

--- cmp-databinding-assignment-start/src/app/app.component.ts
+++ cmp-databinding-assignment-start/src/app/app.component.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  onIntervalStarted(givenNumber: number) {
+    if (givenNumber % 2 === 0) {
+      this.evenNumbers.push(givenNumber);
+    } else {
+      this.oddNumbers.push(givenNumber);
+    }
+  }
 }
--- cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
+++ cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+  @Output() intervalStart = new EventEmitter<number>();
+  interval;
+  lastNumber = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onStartGame() {
+    this.interval = setInterval(() => {
+      this.intervalStart.emit(this.lastNumber + 1);
+      this.lastNumber++;
+    }, 1000)
+  }
+
+  onStopGame() {
+    clearInterval(this.interval);
   }
 
 }
