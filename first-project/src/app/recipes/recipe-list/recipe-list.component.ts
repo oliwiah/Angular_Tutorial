9f94820fcb8bb491770c9667146095641eaa1a06
Oliwia Hajszprych <oliwia.hajszprych@gmail.com>
2018-06-11 17:22:34 +0200

    section 6 finished

--- first-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ first-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +8,25 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'The Description fot it', 'https://get.pxhere.com/photo/dish-meal-food-vegetable-recipe-cuisine-vegetarian-food-parmigiana-1417897.jpg'),
-    new Recipe('Test Recipe', 'The Description fot it', 'https://get.pxhere.com/photo/dish-meal-food-vegetable-recipe-cuisine-vegetarian-food-parmigiana-1417897.jpg')
+    new Recipe(
+      'Test Recipe',
+      'The Description for it',
+      'https://get.pxhere.com/photo/dish-meal-food-vegetable-recipe-cuisine-vegetarian-food-parmigiana-1417897.jpg'
+    ),
+    new Recipe(
+      'Fruit Salad',
+      'Mix all ingredients',
+      'https://c1.staticflickr.com/6/5737/30622968353_35e06fcb52_b.jpg'
+    )
   ];
-  
-  constructor() { }
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  onRecipeSelected(shownRecipe: Recipe) {
+    this.recipeWasSelected.emit(shownRecipe);
   }
-
 }
